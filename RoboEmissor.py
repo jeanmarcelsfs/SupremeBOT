a58745bd9d11398e94e5700549a7a7bb235c2bb9
Jean Barboza <jean_marcel_sfs@hotmail.com>
2022-03-09 09:21:59 -0300

    alterações

--- RoboEmissor.py
+++ RoboEmissor.py
@@ -4,16 +4,17 @@ import pyautogui as pag
 import os
 dataFrame = pd.read_excel(r'C:\Users\jean_\Desktop\_faturar.xlsx')
 
-
 #print(dataFrame)
 qtde = dataFrame['Pedido'].count()
 i = 0
 while i < qtde:
     print(dataFrame.iloc[i][0])
-    caminho = dataFrame.iloc[i][0]
-    caminho = caminho + " - " + dataFrame.iloc[i][6]
+    caminho = str(dataFrame.iloc[i][0])
+    caminho = caminho + " - " + str(dataFrame.iloc[i][6])
+    print(caminho)
+    print("teste de concatenacao"+caminho)
     if not os.path.exists(r'z:/TI/Teste/'+caminho):
-        os.makedirs(r'z:/TI/Teste'+caminho)
+        os.makedirs(r'z:/TI/Teste/'+caminho)
     i = i+1
 print("saiu do laço")
 
